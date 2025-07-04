6b30f0b8446ba1f7ccbc8dae115acff60a8ea531
Matezzi777 <matezzi777@gmail.com>
2025-07-04 10:34:08 +0200

    A bit of cleaning in sooo_long sprites

--- sooooo_long/srcs/collec_sprite.c
+++ sooooo_long/srcs/collec_sprite.c
@@ -6,7 +6,7 @@
 /*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 23:15:15 by maxmart2          #+#    #+#             */
-/*   Updated: 2025/07/04 07:55:30 by maxmart2         ###   ########.fr       */
+/*   Updated: 2025/07/04 10:32:01 by maxmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_bool	load_collec_sprite(t_game *game)
 	int	img_height;
 
 	game->c_sprite = mlx_xpm_file_to_image(game->mlx,
-		"textures/pokeball_test.xpm", &img_width, &img_height);
+		"textures/pokeball.xpm", &img_width, &img_height);
 	if (!game->c_sprite)
 		return (FALSE);
 	return (TRUE);
--- sooooo_long/srcs/exit_sprite.c
+++ sooooo_long/srcs/exit_sprite.c
@@ -6,7 +6,7 @@
 /*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 23:12:02 by maxmart2          #+#    #+#             */
-/*   Updated: 2025/07/04 08:35:44 by maxmart2         ###   ########.fr       */
+/*   Updated: 2025/07/04 10:32:08 by maxmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ t_bool	load_exit_sprite(t_game *game)
 	game->e_sprites = (t_esprites *)malloc(sizeof(t_esprites));
 	if (!game->e_sprites)
 		return (FALSE);
-	game->e_sprites->close = mlx_xpm_file_to_image(game->mlx, "textures/exit_close_test.xpm",
+	game->e_sprites->close = mlx_xpm_file_to_image(game->mlx, "textures/exit_close.xpm",
 		&img_width, &img_height);
 	if (!game->e_sprites->close)
 		return (FALSE);
-	game->e_sprites->open = mlx_xpm_file_to_image(game->mlx, "textures/exit_open_test.xpm",
+	game->e_sprites->open = mlx_xpm_file_to_image(game->mlx, "textures/exit_open.xpm",
 		&img_width, &img_height);
 	if (!game->e_sprites->open)
 		return (FALSE);
